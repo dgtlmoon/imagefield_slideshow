e161671128b1d1e2ea3a4355b33a92c6d1e036b2
heykarthikwithu <heykarthikwithu@gmail.com>
2016-04-27 17:06:05 +0530

    Basic slider for the image field is been implemented.

--- src/Plugin/Field/FieldFormatter/ImagefieldSlideshowFieldFormatter.php
+++ src/Plugin/Field/FieldFormatter/ImagefieldSlideshowFieldFormatter.php
@@ -8,6 +8,7 @@ use Drupal\Core\Field\FieldItemInterface;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Url;
+use Drupal\image\Entity\ImageStyle;
 use Drupal\image\Plugin\Field\FieldFormatter\ImageFormatterBase;
 
 /**
@@ -63,60 +64,18 @@ class ImagefieldSlideshowFieldFormatter extends ImageFormatterBase {
       return $elements;
     }
 
-    $url = NULL;
-    $image_link_setting = $this->getSetting('image_link');
-    // Check if the formatter involves a link.
-    if ($image_link_setting == 'content') {
-      $entity = $items->getEntity();
-      if (!$entity->isNew()) {
-        $url = $entity->urlInfo();
-      }
-    }
-    elseif ($image_link_setting == 'file') {
-      $link_file = TRUE;
-    }
-
-    $image_style_setting = $this->getSetting('image_style');
-
-    // Collect cache tags to be added for each item in the field.
-    $cache_tags = array();
-    if (!empty($image_style_setting)) {
-      // $image_style = $this->imageStyleStorage->load($image_style_setting);
-      // $cache_tags = $image_style->getCacheTags();
-    }
-
+    $image_uri_values = [];
     foreach ($files as $delta => $file) {
-      $cache_contexts = array();
-      if (isset($link_file)) {
-        $image_uri = $file->getFileUri();
-        // @todo Wrap in file_url_transform_relative(). This is currently
-        // impossible. As a work-around, we currently add the 'url.site' cache
-        // context to ensure different file URLs are generated for different
-        // sites in a multisite setup, including HTTP and HTTPS versions of the
-        // same site. Fix in https://www.drupal.org/node/2646744.
-        $url = Url::fromUri(file_create_url($image_uri));
-        $cache_contexts[] = 'url.site';
-      }
-      $cache_tags = Cache::mergeTags($cache_tags, $file->getCacheTags());
+      $uri = $file->getFileUri();
+      $image_uri_value = ImageStyle::load('medium')->buildUrl($uri);
 
-      // Extract field item attributes for the theme function, and unset them
-      // from the $item so that the field template does not re-render them.
-      $item = $file->_referringItem;
-      $item_attributes = $item->_attributes;
-      unset($item->_attributes);
-
-      $elements[$delta] = array(
-        '#theme' => 'image_formatter',
-        '#item' => $item,
-        '#item_attributes' => $item_attributes,
-        // '#image_style' => $image_style_setting,
-        '#url' => $url,
-        '#cache' => array(
-          'tags' => $cache_tags,
-          'contexts' => $cache_contexts,
-        ),
-      );
+      $a = '';
+      $image_uri_values[] = $image_uri_value;
     }
+    $elements[] = array(
+      '#theme' => 'imagefield_slideshow',
+      '#url' => $image_uri_values,
+    );
 
     /*foreach ($items as $delta => $item) {
       $elements[$delta] = ['#markup' => $this->viewValue($item)];
